6c14079715187a4d512be134dcbbf1f586928f64
Gustavo <162714951+gustavodaque@users.noreply.github.com>
2024-08-02 12:04:50 -0300

    Add files via upload

---
+++ exercicio1.js
@@ -0,0 +1,10 @@
+let frase = "JavaScript é incrível!"
+//metodo lenght que conta o numero de caracteres
+let comprimento = frase.lenght
+console.log(comprimento)
+
+let palavra = frase.slice(13 ,21)
+console.log(palavra)
+
+let substituir = frase.replace("incrível!", "Fantástico")
+console.log(substituir)
